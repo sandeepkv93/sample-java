5fdcceb1521f618c467e0f4493314cf33c4132a7
Sandeep Vishnu <sandeepkv93@gmail.com>
2020-08-13 21:45:00 -0700

    Method to add two numbers

--- Arithmetic.java
+++ Arithmetic.java
@@ -1,5 +1,12 @@
 public class Arithmetic {
+    static int addTwoNumbers(int x, int y) {
+        return x + y;
+    }
+
     public static void main(String[] args) {
         System.out.println("Hello");
+        int a = 10;
+        int b = 20;
+        System.out.println("Sum of " + a + " and " + b + " is: " + addTwoNumbers(a, b));
     }
 }
