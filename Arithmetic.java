e3699498ece833238702e6cbef38b8ff3108ad1b
Sandeep Vishnu <sandeepkv93@gmail.com>
2020-08-13 22:20:36 -0700

    Product of two numbers

--- Arithmetic.java
+++ Arithmetic.java
@@ -7,11 +7,16 @@ public class Arithmetic {
         return x - y;
     }
 
+    static int multiplyTwoNumbers(int x, int y) {
+        return x * y;
+    }
+
     public static void main(String[] args) {
         System.out.println("Hello");
         int a = 10;
         int b = 20;
         System.out.println("Sum of " + a + " and " + b + " is: " + addTwoNumbers(a, b));
         System.out.println("Diff of " + a + " and " + b + " is: " + subtractTwoNumbers(a, b));
+        System.out.println("Product of " + a + " and " + b + " is: " + multiplyTwoNumbers(a, b));
     }
 }
